e0ce99dfa31c6da1d56d22ec3d3c550eb1aa4645
Tennyleaz <tennyleaz@gmail.com>
2018-08-28 10:11:19 +0800

    Updated project, add Excel export and HTTP report objects.

--- PiwikClientTest/Properties/AssemblyInfo.cs
+++ PiwikClientTest/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // 組件的一般資訊是由下列的屬性集控制。
 // 變更這些屬性的值即可修改組件的相關
 // 資訊。
-[assembly: AssemblyTitle("PiwikClientTest")]
+[assembly: AssemblyTitle("Piwik Client Test App")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PiwikClientTest")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright Tenny ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
